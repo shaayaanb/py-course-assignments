ac8608cbd944487536b535560de8a63d1a0efa08
shaayaanb <261874801+shaayaanb@users.noreply.github.com>
2026-02-19 14:57:25 +0330

    import additional functions and use them in other parts of the class

--- 3) Stack .py
+++ 3) Stack .py
@@ -1,38 +1,85 @@
-# ---------------------------------------------
-# پیاده سازی پشته
-# ---------------------------------------------
-
 class Stack:
 
     def __init__(self, limit=100):
         self.stack = []
         self.limit = limit
 
-    def push(self, x):
-        if len(self.stack) >= self.limit:
-            print("stack is full")
-            return
-        self.stack.append(x)
+    def push(self, data ):
+        self.FS_errCheck()
+        self.stack.append(data)
 
     def pop(self):
-        if len(self.stack) == 0:
-            print("stack is empty")
-            return
+        self.ES_errCheck()
         return self.stack.pop()
 
     def peek(self):
-        if len(self.stack) == 0:
-            print("stack is empty")
-            return
+        self.ES_errCheck()
         return self.stack[-1]
 
-    def find(self, x):
-        for i in range(len(self.stack)):
+    def find(self ,x ):
+        for i in range(self.size()):
             if self.stack[i] == x:
                 return i
         return -1
 
-    def replace(self, x, y):
-        for i in range(len(self.stack)):
+    def replace_all(self ,x ,y ):
+        for i in range(self.size()):
             if self.stack[i] == x:
                 self.stack[i] = y
+
+    #↑↑↑↑↑↑↑↑↑↑↑↑↑↑_Class  methods_↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    #--------------------------------------------
+    #↓↓↓↓↓↓↓↓↓↓↓↓↓↓_Custom methods_↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+    def is_empty(self):
+        return self.size() == 0
+    
+    def is_full(self):
+        return self.size() >= self.limit
+
+    def FS_errCheck(self):
+        if self.size() >= self.limit:
+            raise IndexError ("stack is full")
+
+    def ES_errCheck(self):
+        if self.size() == 0:
+            raise IndexError ("stack is empty")
+    
+    def size(self):
+        return len(self.stack)
+
+    def find_middle(self):
+        self.ES_errCheck()
+        return self.stack[self.size()//2]
+
+    def clear(self):
+        self.stack.clear()
+
+    def replace_first(self ,x ,y ):
+        indx = self.find(x)
+        if indx != -1:
+            self.stack[indx] = y
+        return -1
+
+    def show(self):
+        for i in range(self.size() -1, -1 ,-1 ):
+            print(self.stack[i] ,end=" " )
+        
+    def display(self):
+        for i in range(self.size()):
+            print(self.stack[i] ,end=" " )
+
+    def rev_stack(self):
+
+        S1 = Stack(self.limit)
+        S2 = Stack(self.limit)
+
+        while self.size() > 0 :
+            S1.push(self.pop())
+
+        while S1.size() > 0 :
+            S2.push(S1.pop())
+
+        while S2.size() > 0 :
+            self.push(S2.pop())
+
