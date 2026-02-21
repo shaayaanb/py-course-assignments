d4608b3780007054d19967471516244280455d5e
shaayaanb <261874801+shaayaanb@users.noreply.github.com>
2026-02-21 23:21:19 +0330

    separate and complete Queue types

--- 4) Queue .py
+++ 4) Queue .py
@@ -1,77 +1,41 @@
-# ---------------------------------------------
-# صف ساده
-# ---------------------------------------------
-
 class Queue:
 
-    def __init__(self, max_size=100):
-        self.list = [None] * max_size
+    def __init__(self, max=100):
+        self.Q = [None] * max
         self.front = -1
         self.rear = -1
+        self.max = max
 
-    def insert(self, x):
-        if self.rear >= len(self.list) - 1:
-            print("Queue is Full")
-            return
-
+    def insert(self ,x ):
+        self.FQ_errCheck()
         if self.front == -1:
             self.front = 0
-            self.rear = 0
-        else:
-            self.rear += 1
-
-        self.list[self.rear] = x
+        self.rear += 1
+        self.Q[self.rear] = x
 
     def delete(self):
-        if self.front == -1:
-            print("Queue is empty")
-            return
-
-        value = self.list[self.front]
-
+        self.EQ_errCheck()
+        value = self.Q[self.front]
         if self.front == self.rear:
             self.front = -1
             self.rear = -1
         else:
             self.front += 1
-
         return value
 
-# ---------------------------------------------
-# صف حلقوی
-# ---------------------------------------------
-
-class CircularQueue:
-
-    def __init__(self, max_size=100):
-        self.list = [None] * max_size
-        self.front = -1
-        self.rear = -1
-
-    def insert(self, x):
-        if (self.rear + 1) % len(self.list) == self.front:
-            print("Queue is full")
+    def show(self):
+        if self.front == -1 or self.front > self.rear:
+            print("empty queue")
             return
 
-        if self.front == -1:
-            self.front = 0
-            self.rear = 0
-        else:
-            self.rear = (self.rear + 1) % len(self.list)
+        for i in range(self.front, self.rear + 1):
+            print(self.Q[i], end=" ")
+        print()
 
-        self.list[self.rear] = x
+    def EQ_errCheck(self):
+        if self.front == -1 or self.front > self.rear:
+            raise IndexError("Queue is empty")
 
-    def delete(self):
-        if self.front == -1:
-            print("Queue is empty")
-            return
-
-        value = self.list[self.front]
-
-        if self.front == self.rear:
-            self.front = -1
-            self.rear = -1
-        else:
-            self.front = (self.front + 1) % len(self.list)
-
-        return value
+    def FQ_errCheck(self):
+        if self.rear == self.max - 1:
+            raise IndexError ("Queue is full")
